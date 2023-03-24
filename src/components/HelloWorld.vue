3172fc8fc6e1d7d4f9376a576b2c14e83aa152bd
Savelyev Alexander <savelevav@pik.ru>
2023-03-24 12:41:55 +0300

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,99 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-treeview
+    v-model="tree"
+    :open="open"
+    :items="items"
+    activatable
+    item-key="name"
+    open-on-click
+  >
+    <template v-slot:prepend="{ item, open }">
+      <v-icon v-if="!item.file">
+        {{ open ? "mdi-folder-open" : "mdi-folder" }}
+      </v-icon>
+      <v-icon v-else>
+        {{ files[item.file] }}
+      </v-icon>
+    </template>
+  </v-treeview>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data: () => ({
+    open: ["public", "static"],
+    files: {
+      html: "mdi-language-html5",
+      js: "mdi-nodejs",
+      json: "mdi-json",
+      md: "mdi-markdown",
+      pdf: "mdi-file-pdf",
+      png: "mdi-file-image",
+      txt: "mdi-file-document-outline",
+      xls: "mdi-file-excel",
+    },
+    tree: [],
+    items: [
+      {
+        name: ".git",
+      },
+      {
+        name: "node_modules",
+        children: [
+          {
+            name: "logo.png",
+            file: "png",
+          },
+        ],
+      },
+      {
+        name: "public",
+        children: [
+          {
+            name: "static",
+            children: [
+              {
+                name: "logo.png",
+                file: "png",
+              },
+            ],
+          },
+          {
+            name: "favicon.ico",
+            file: "png",
+          },
+          {
+            name: "index.html",
+            file: "html",
+          },
+        ],
+      },
+      {
+        name: ".gitignore",
+        file: "txt",
+      },
+      {
+        name: "babel.config.js",
+        file: "js",
+      },
+      {
+        name: "package.json",
+        file: "json",
+      },
+      {
+        name: "README.md",
+        file: "md",
+      },
+      {
+        name: "vue.config.js",
+        file: "js",
+      },
+      {
+        name: "yarn.lock",
+        file: "txt",
+      },
+    ],
+  }),
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
